751eb0c2ac719e1cbbe9fec7a6f06c6531f77a4d
cunnia3 <andrew64ce@gmail.com>
2015-11-06 16:54:27 -0500

    Added PID control and combined duty cycle commands for clean code

--- roboClawRR/roboclawClient.py
+++ roboClawRR/roboclawClient.py
@@ -11,20 +11,18 @@ def main():
     RRN.RegisterTransport(t2)
 	
     # example connect string
-    myRoboClaw = RRN.ConnectService('tcp://localhost:54521/roboClawController/wheelChairControl')
+    myRoboClaw = RRN.ConnectService('tcp://localhost:48338/roboClawController/wheelChairControl')
 
 
     # go forward for 3 seconds, send a command 20 times a second to prevent auto stop
     timeNow = time.time()
     myRoboClaw.setDutyMax(4500)
-    while time.time() - timeNow < 3:
-        myRoboClaw.setM1(int(4500))
-        myRoboClaw.setM2(int(4500))
+    while time.time() - timeNow < 10:
+        #myRoboClaw.setMDuties(int(8500),int(10000))
+        myRoboClaw.setMVelocities(15,15)
         time.sleep(.05)
     
     print "stopping"
-    myRoboClaw.setM1(0)
-    myRoboClaw.setM2(0)
 
     RRN.Shutdown()
   
--- roboClawRR/roboclawRR_server.py
+++ roboClawRR/roboclawRR_server.py
@@ -10,6 +10,7 @@ import sys
 import serial
 import struct
 import roboclaw
+from PID import PID
 
 
 # TO USE
@@ -19,7 +20,7 @@ import roboclaw
 
 
 address = 0x80
-roboclaw.Open("/dev/ttyACM2",115200)
+roboclaw.Open("/dev/ttyACM1",115200)
 lastMessageTime = time.time()
 
 
@@ -29,37 +30,49 @@ class RoboClawState:
         self.m1Duty = 0
         self.m2Duty = 0
         #largest absolute value that the motors can be set to
-        self.dutyMax = 4000
+        self.dutyMax = 10000
+        
+        # control mode will be either Duty or Velocity depending on last command sent
+        self.controlMode = 'Duty'
+        
+        self.canMeasureWheelVelocities = True
+        
+        self.pidControllerR = PID(-50,0,0,0,0,0,0)
+        self.pidControllerL = PID(-50,0,0,0,0,0,0)
+        
+        # try to find and connect to NRF IMU server to get wheel velocities
+        try:
+            self.imuGateway = RRN.ConnectService('tcp://localhost:39085/arduinoIMU/arduinoIMUData')
+        except:
+            print "Couldn't find NRF IMU server, unable to accept velocity commands!"
+            self.canMeasureWheelVelocities = False
         self._lock = threading.RLock()
         
      
     def setDutyMax(self, newMax):          
         self.dutyMax = newMax
 
-    def setM1(self, newDuty):
-	global lastMessageTime
-	lastMessageTime = time.time()
-        if abs(newDuty) > self.dutyMax:
-            self.m1Duty = self.dutyMax * cmp(newDuty, 0)
-            return
-
+    def setMDuties(self, leftDuty, rightDuty):
+        self.controlMode = 'Duty'
+        global lastMessageTime
+        lastMessageTime = time.time()
+        if abs(leftDuty) > self.dutyMax:
+            self.m1Duty = self.dutyMax * cmp(leftDuty, 0)
         else:
-            self.m1Duty = newDuty
+            self.m1Duty = leftDuty
 
-    def setM2(self, newDuty):
-	global lastMessageTime
-	lastMessageTime = time.time()
-	if abs(newDuty) > self.dutyMax:
-            self.m2Duty = self.dutyMax * cmp(newDuty, 0)
-            return
-
+    	if abs(rightDuty) > self.dutyMax:
+            self.m2Duty = self.dutyMax * cmp(rightDuty, 0)
         else:
-            self.m2Duty = newDuty
-
-    def writePulseWidths(self):
-        roboclaw.DutyAccelM1(address,5000,int(self.m1Duty))
-        roboclaw.DutyAccelM2(address,5000,int(self.m2Duty))
-
+            self.m2Duty = rightDuty
+            
+    def setMVelocities(self, leftV,rightV):
+        self.controlMode = 'Velocity'
+        global lastMessageTime
+        lastMessageTime = time.time()
+        #print leftV,rightV
+        self.pidControllerL.setPoint(leftV)
+        self.pidControllerR.setPoint(rightV)
 
 
 #********************************#
@@ -100,14 +113,27 @@ def main():
     # update duty cycle at 20Hz
     # stop wheelchair if a command hasnt been received in the last 1/4 second
     while 1:
-        if time.time() - lastMessageTime < .250:
-            print myRoboClaw.m1Duty, myRoboClaw.m2Duty
-            roboclaw.DutyAccelM1(address,5000,int(myRoboClaw.m1Duty))
-            roboclaw.DutyAccelM2(address,5000,int(myRoboClaw.m2Duty))
-        
-        else:
+    
+        if time.time() - lastMessageTime > .250:
             roboclaw.DutyAccelM1(address,30000,0)
             roboclaw.DutyAccelM2(address,30000,0)
+            continue
+            
+        if myRoboClaw.controlMode == 'Duty':
+            roboclaw.DutyAccelM1(address,5000,int(myRoboClaw.m1Duty))
+            roboclaw.DutyAccelM2(address,5000,int(myRoboClaw.m2Duty))
+            
+        if myRoboClaw.controlMode == 'Velocity':
+            leftSpeed= myRoboClaw.imuGateway.IMU1_read()[5]
+            rightSpeed = myRoboClaw.imuGateway.IMU2_read()[5]
+            leftCommand = myRoboClaw.pidControllerL.update(leftSpeed) + myRoboClaw.m1Duty 
+            rightCommand = myRoboClaw.pidControllerR.update(rightSpeed) + myRoboClaw.m2Duty 
+            #print leftCommand
+            print rightCommand, rightSpeed
+            myRoboClaw.setMDuties(leftCommand, rightCommand)
+            roboclaw.DutyAccelM1(address,5000,int(myRoboClaw.m1Duty))
+            roboclaw.DutyAccelM2(address,5000,int(myRoboClaw.m2Duty))
+            
         
         time.sleep(.05)
 
